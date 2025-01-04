a53d576e76edbc875e1424d4db0656d2c92f51e3
Ali Adel <alya70934@gmail.com>
2025-01-04 23:44:53 +0200

    V2

--- app.js
+++ app.js
@@ -7,6 +7,4 @@ app.get("/hello", (req, res) => {
 	res.status(200).send("Hello World I Am Ali Adel Ahmed");
 });
 
-app.listen(3000, () => {
-	console.log("Server Is Running On Port 5000");
-});
+module.exports = app;
